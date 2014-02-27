e90f18d7210ec9a8ff662a5b67139d90b3def468
Techiemouse <fireflyangerona@gmail.com>
2014-02-27 17:57:13 +0000

    Modified Tagger verbs class to extract only verbs from the tagger sentence. Replaced the tags for verbs with spaces and eliminated the words that contain other tags (by checking for _ ) resulting with only verbs from the given text.

--- NewsArticleAnalysis/src/uk/ac/aber/dst1/newsarticleanalysis/Main.java
+++ NewsArticleAnalysis/src/uk/ac/aber/dst1/newsarticleanalysis/Main.java
@@ -4,9 +4,9 @@ public class Main {
 	
 	 public static void main(String[] args) throws Exception {
 		 
-		URLConnectionReader urlConnect = new URLConnectionReader();	 
-		urlConnect.getArticles();
-		 //TaggerVerbs tagg = new TaggerVerbs();
-		 //tagg.taggIT();
+		//URLConnectionReader urlConnect = new URLConnectionReader();	 
+		//urlConnect.getArticles();
+		 TaggerVerbs tagg = new TaggerVerbs();
+		tagg.taggIT();
 	 }
 }
